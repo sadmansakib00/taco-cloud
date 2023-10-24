6c3a2dd935ad8a20fbd40e85a2b3754ae39f2f18
sadmansakib00 <sakibs100@gmail.com>
2023-10-25 03:09:55 +0600

    Adds post mapping for taco order

--- src/main/java/com/shop/tacocloud/controller/OrderController.java
+++ src/main/java/com/shop/tacocloud/controller/OrderController.java
@@ -1,10 +1,13 @@
 package com.shop.tacocloud.controller;
 
+import com.shop.tacocloud.model.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 /**
  * Author: sakib
@@ -21,6 +24,13 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 
 }
