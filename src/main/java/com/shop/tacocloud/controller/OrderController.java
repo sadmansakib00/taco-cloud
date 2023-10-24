01703508a4eda338a0aa5d719a27aa3558ccfe5f
sadmansakib00 <sakibs100@gmail.com>
2023-10-25 03:02:39 +0600

    Adds order controller and order form

---
+++ src/main/java/com/shop/tacocloud/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.shop.tacocloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+/**
+ * Author: sakib
+ * Date: 25/10/2023
+ * Description:
+ */
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+}
