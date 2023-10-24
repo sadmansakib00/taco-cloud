e4443eb3cba0d1e65e0c5d3005fc34d84e16dbb7
sadmansakib00 <sakibs100@gmail.com>
2023-10-25 02:00:58 +0600

    Adds post mapping for taco design

--- src/main/java/com/shop/tacocloud/controller/DesignTacoController.java
+++ src/main/java/com/shop/tacocloud/controller/DesignTacoController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -56,6 +57,13 @@ public class DesignTacoController {
     @RequestMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
