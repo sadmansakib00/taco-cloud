a6538d42a06f542b954bc1e72d82dcb02f2c0bd2
sadmansakib00 <sakibs100@gmail.com>
2023-10-28 02:30:00 +0600

    Updates the design controller and converter classes to use the DB instead of hard coded data

--- src/main/java/com/shop/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/shop/tacocloud/controllers/DesignTacoController.java
@@ -4,8 +4,10 @@ import com.shop.tacocloud.models.Ingredient;
 import com.shop.tacocloud.models.Ingredient.Type;
 import com.shop.tacocloud.models.Taco;
 import com.shop.tacocloud.models.TacoOrder;
+import com.shop.tacocloud.repositories.IngredientRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -24,25 +26,20 @@ import java.util.stream.Collectors;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
-
-        Type[] types = Type.values();
-        for (Type type : types) {
+        Iterable<Ingredient> ingredients = ingredientRepository.findAll();
+        Type[] types = Ingredient.Type.values();
+        for(Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
+                    filterByType((List<Ingredient>) ingredients, type));
         }
     }
 
