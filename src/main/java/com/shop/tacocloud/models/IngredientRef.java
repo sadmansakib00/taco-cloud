52e863efb1dbc4875c35288d3eaacbb34c735214
sadmansakib00 <sakibs100@gmail.com>
2023-10-30 02:54:13 +0600

    Adds ingredient Ref as an entity

---
+++ src/main/java/com/shop/tacocloud/models/IngredientRef.java
@@ -0,0 +1,16 @@
+package com.shop.tacocloud.models;
+
+import lombok.Data;
+
+/**
+ * Author: sakib
+ * Date: 30/10/2023
+ * Description:
+ */
+
+@Data
+public class IngredientRef {
+
+    private final String ingredient;
+
+}
